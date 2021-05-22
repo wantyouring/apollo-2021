bdfd8dc504832ca67ecf7ee8091c1532937a8dc7
wantyouring <pwc99@naver.com>
2021-05-22 18:33:25 +0900

    1.1 GET_MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,23 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import React from 'react';
-export default () => <h1>Home</h1>;
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return 'loading';
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.id}</h1>);
+  }
+};
