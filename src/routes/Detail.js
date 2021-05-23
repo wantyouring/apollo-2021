7fdd2fc7b60a13ccf1d7652b41a39b77d5833bc6
wantyouring <pwc99@naver.com>
2021-05-23 13:44:11 +0900

    1.3 Apollo Cache and Styles

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,52 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import { useParams } from 'react-router';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
-      id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
 `;
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -19,10 +55,14 @@ export default () => {
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: parseInt(id) },
   });
-  if (loading) {
-    return 'loading';
-  }
-  if (data && data.movie) {
-    return data.movie.title;
-  }
+  return (
+    <Container>
+      <Column>
+        <Title>Name</Title>
+        <Subtitle>English · 4.5</Subtitle>
+        <Description>lorem ipsum lalalla </Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
+  );
 };
